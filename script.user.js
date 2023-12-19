7b8e1c8244d8643b0dc641efac7ec8944c1ce92f
Josh Pigford <josh@joshpigford.com>
2023-12-19 16:12:15 -0600

    Wantlist/FSFT + Price Linking

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TCDb Enhancerizer
 // @namespace    https://tcdb.com/
-// @version      0.2
+// @version      0.3
 // @description  Adds new tools to get more out of Trading Card Database!
 // @author       Shpigford
 // @match        https://www.tcdb.com/*
@@ -18,58 +18,87 @@
 (function () {
   'use strict';
 
-  var total1 = 0;
-  var total2 = 0;
+  // Limit to Transaction pages
+  if (window.location.href.indexOf("YourTransactions.cfm") > -1) {
+    // Make main content area full width.
 
-  var tableRows1 = $('#content > div.col-md-9.nopadding > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(1) > table.table.table-borderless.table-sm > tbody > tr');
-  var tableRows2 = $('#content > div.col-md-9.nopadding > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > table.table.table-borderless.table-sm > tbody > tr');
+    $('#content > div.col-md-9.nopadding').removeClass('col-md-9').addClass('col-md-12');
+    $('#content > div.col-md-3.nopadding').remove();
 
-  function calculateTotal(rows, total, callback) {
-    var count = rows.length;
+    // Add bottom border to table rows
+    $('#content > div > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(1) > table.table.table-borderless.table-sm > tbody > tr').css('border-bottom', '1px solid #ddd');
+    $('#content > div > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > table.table.table-borderless.table-sm > tbody > tr').css('border-bottom', '1px solid #ddd');
 
-    rows.each(function () {
-      var row = $(this); // Save the current row
-      var url = row.find('a[href^="/ViewCard"]').attr('href');
+    // Alternate row colors
+    $('#content > div > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(1) > table.table.table-borderless.table-sm > tbody > tr:nth-child(even)').css('background-color', '#eee');
+    $('#content > div > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > table.table.table-borderless.table-sm > tbody > tr:nth-child(even)').css('background-color', '#eee');
 
-      GM_xmlhttpRequest({
-        method: "GET",
-        url: "https://www.tcdb.com" + url,
-        onload: function (response) {
-          var responseHTML = response.responseText;
-          var responseDOM = $(jQuery.parseHTML(responseHTML));
+    // Build totals
+    var total1 = 0;
+    var total2 = 0;
 
-          var priceElement = $('li > a[href^="/Price.cfm"]', responseHTML);
-          var price = priceElement.length > 0 ? priceElement.text() : '0';
+    var tableRows1 = $('#content > div.col-md-12.nopadding > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(1) > table.table.table-borderless.table-sm > tbody > tr');
+    var tableRows2 = $('#content > div.col-md-12.nopadding > div:nth-child(1) > table.table.table-bordered > tbody > tr:nth-child(2) > td:nth-child(2) > table.table.table-borderless.table-sm > tbody > tr');
 
-          // Remove any non-numeric characters from the price
-          price = parseFloat(price.replace(/[^0-9\.]/g, ''));
+    function calculateTotal(rows, total, callback) {
+      var count = rows.length;
 
-          // Add the price to the total
-          total += price;
+      rows.each(function () {
+        var row = $(this); // Save the current row
+        var url = row.find('a[href^="/ViewCard"]').attr('href');
 
-          // Append the price to the current row
-          price = price.toFixed(2);
-          if (price == 0) {
-            row.append('<td style="text-align:right; color:gray;width:80px;">No price</td>');
-          } else {
-            row.append('<td style="text-align:right;color:green;">$' + price + '</td>');
+        GM_xmlhttpRequest({
+          method: "GET",
+          url: "https://www.tcdb.com" + url,
+          onload: function (response) {
+            var responseHTML = response.responseText;
+            var responseDOM = $(jQuery.parseHTML(responseHTML));
+
+            var priceElement = $('li > a[href^="/Price.cfm"]', responseHTML);
+            var price = priceElement.length > 0 ? priceElement.text() : '0';
+
+            // Get the price element link/url
+            var priceLink = priceElement.attr('href');
+
+            // Remove any non-numeric characters from the price
+            price = parseFloat(price.replace(/[^0-9\.]/g, ''));
+
+            // Add the price to the total
+            total += price;
+
+            // Wantlist
+            var wantlists = parseInt(responseDOM.find('span[title="Wantlists"]').text(), 10);
+            wantlists = '<span class="badge bg-danger" title="Wantlists">' + wantlists + '</span>';
+
+            // FSFT
+            var fsft = parseInt(responseDOM.find('span[title="For Sale / Trade"]').text(), 10);
+            fsft = '<span class="badge bg-success" title="For Sale / Trade">' + fsft + '</span>';
+
+            // Append the price to the current row
+            price = price.toFixed(2);
+
+            if (price == 0) {
+              row.append('<td style="text-align:right; color:gray;width:80px;">No price<br>' + wantlists + ' ' + fsft + '</td>');
+            } else {
+              row.append('<td style="text-align:right;width:80px;"><a href="' + priceLink + '" class="text-primary fw-bold">$' + price + '</a><br>' + wantlists + ' ' + fsft + '</td>');
+            }
+
+            count--;
+            if (count === 0) {
+              total = total.toFixed(2);
+              callback(total);
+            }
           }
-
-          count--;
-          if (count === 0) {
-            total = total.toFixed(2);
-            callback(total);
-          }
-        }
+        });
       });
+    }
+
+    calculateTotal(tableRows1, total1, function (total) {
+      $('em:contains("item(s)")').eq(0).after("<strong class='float-end badge text-light bg-success'>Total: $" + total + "</strong>");
+    });
+
+    calculateTotal(tableRows2, total2, function (total) {
+      $('em:contains("item(s)")').eq(1).after("<strong class='float-end badge text-light bg-success'>Total: $" + total + "</strong>");
     });
   }
-
-  calculateTotal(tableRows1, total1, function (total) {
-    $('em:contains("item(s)")').eq(0).after("<strong class='float-end badge text-light bg-success'>Total: $" + total + "</strong>");
-  });
-
-  calculateTotal(tableRows2, total2, function (total) {
-    $('em:contains("item(s)")').eq(1).after("<strong class='float-end badge text-light bg-success'>Total: $" + total + "</strong>");
-  });
 })();
